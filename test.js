c9f81aeb66ed30e945e1a38220a872e96deb602b
GaneshRoyal07 <kondetiganesh43@gmail.com>
2026-01-30 12:12:44 +0530

    basic js

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
 console.log("hlo world")
+ let a=10;
+ let b=-3;
+ let c= 'hii';
+ total=a+b+" "+c;
+ console.log(total);
+
+ let x=4;
+ let y=5;
+ const multiply=x*y;
+ console.log(multiply);
+ const square=x**y;
+ console.log(square);
+
+ function sayHlo(){
+    return 'hlo';
+ }
+ let greeting= sayHlo();
+ console.log(greeting);
+
+ let array=['hii', 1.2, 1 ];
+ console.log(array);
+ let array1=array[0]+ array[1];
+ console.log(array1);
+ 
